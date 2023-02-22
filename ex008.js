3e8a561dfd0261dbe50651ff7d918395235d755f
gabriel-sady <119871891+gabriel-sady@users.noreply.github.com>
2023-02-22 20:44:23 -0300

    js

---
+++ ex008.js
@@ -0,0 +1,7 @@
+var vel = 80
+console.log(`A velocidade do carro é ${vel}km/h`)
+if (vel > 80) {
+    console.log('Você foi MUTADO!')
+} else {
+    console.log('Dirija usando o cinto de segurança')
+}
---
+++ ex009.js
@@ -0,0 +1,5 @@
+var pais = 'Brasil'
+console.log(`Você mora no ${pais}`)
+if (pais == 'Brasil'){
+    console.log('Você é Brasileiro')
+} else { console.log('Você é extrangeiro')}
